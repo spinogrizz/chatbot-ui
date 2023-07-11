148f2ad4ea9e98d0b07cf50918b6cc04f11cbc44
Gryzlov Denis <gryzlov@gmail.com>
2023-07-11 23:53:16 +0300

    hide some settings

--- components/Chatbar/components/ChatbarSettings.tsx
+++ components/Chatbar/components/ChatbarSettings.tsx
@@ -50,17 +50,17 @@ export const ChatbarSettings = () => {
         onClick={() => handleExportData()}
       />
 
-      <SidebarButton
+      {/* <SidebarButton
         text={t('Settings')}
         icon={<IconSettings size={18} />}
         onClick={() => setIsSettingDialog(true)}
-      />
+      /> */}
 
-      {!serverSideApiKeyIsSet ? (
+      {/* {!serverSideApiKeyIsSet ? (
         <Key apiKey={apiKey} onApiKeyChange={handleApiKeyChange} />
       ) : null}
 
-      {!serverSidePluginKeysSet ? <PluginKeys /> : null}
+      {!serverSidePluginKeysSet ? <PluginKeys /> : null} */}
 
       <SettingDialog
         open={isSettingDialogOpen}
