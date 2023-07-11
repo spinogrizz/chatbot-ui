0ce18034f43d50b025fab764c2a342804483735f
Gryzlov Denis <gryzlov@gmail.com>
2023-07-11 23:45:54 +0300

    hide system prompt UI

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -396,7 +396,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
             ref={chatContainerRef}
             onScroll={handleScroll}
           >
-            {selectedConversation?.messages.length === 0 ? (
+            {false ? (
               <>
                 <div className="mx-auto flex flex-col space-y-5 md:space-y-10 px-3 pt-5 md:pt-12 sm:max-w-[600px]">
                   <div className="text-center text-3xl font-semibold text-gray-800 dark:text-gray-100">
