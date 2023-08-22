313d21eeaa75dcf392932cc155aecbedda57b977
Gryzlov Denis <gryzlov@gmail.com>
2023-08-22 12:40:25 +0300

    fixed models definition

--- types/openai.ts
+++ types/openai.ts
@@ -8,11 +8,11 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-//  GPT_3_5 = 'gpt-3.5-turbo',
+  GPT_3_5 = 'gpt-3.5-turbo',
   GPT_3_5_16K = 'gpt-3.5-turbo-16k',
   GPT_3_5_AZ = 'gpt-35-turbo',
-//  GPT_4 = 'gpt-4',
-//  GPT_4_32K = 'gpt-4-32k',
+  GPT_4 = 'gpt-4',
+  GPT_4_32K = 'gpt-4-32k',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
